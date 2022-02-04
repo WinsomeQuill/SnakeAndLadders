fc6a653c96d8e00aae61ba1fbe90e69bec6fae36
Aviers7 <99005850+Aviers7@users.noreply.github.com>
2022-02-04 14:53:43 +0700

    SQLCommand(3)
    1 - Update Score
    2 - Create Users
    3 - Search Users

---
+++ SQLCommands.sql
@@ -0,0 +1,9 @@
+Update Users
+set Score = 0  
+where Login = '' 
+---------------------------------------------
+insert into Users (Login) values ('Name')
+---------------------------------------------
+Select Score
+from Users
+Where Login = ''
