397f5c494293760480591d8d40fa2f94ed53893b
WinsomeQuill <artemqa.belyakov07@gmail.com>
2022-02-05 12:22:48 +0700

    Fix Player and Utils Updated DevTools

--- SnakesAndLadders/Player.cs
+++ SnakesAndLadders/Player.cs
@@ -5,44 +5,115 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SnakesAndLadders
 {
     public class Player
     {
-        public Player(string name)
+        public Player(string name, VerticalAlignment vertical, HorizontalAlignment horizantal)
         {
             this._Name = name;
             this._label = new Label
             {
                 Content = $"{this._Name}",
-                VerticalAlignment = VerticalAlignment.Top,
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
                 Margin = new Thickness(0, 10, 0, 10),
                 FontSize = 12,
                 FontWeight = FontWeights.Bold,
             };
+            this._VerticalAlignment = vertical;
+            this._HorizontalAlignment = horizantal;
             Utils._map.GridMap.Children.Add(this._label);
-            SetPosition(1);
         }
 
         public string _Name { get; set; }
         public int[] _Position { get; set; } = { 0, 0 };
-        public int _Position_Cage { get; set; } = 0;
+        public int _Position_Cage { get; set; } = 1;
+        public VerticalAlignment _VerticalAlignment { get; set; }
+        public HorizontalAlignment _HorizontalAlignment { get; set; }
         private Label _label { get; set; } // метка игрока, пока это просто текст
 
-        public void SetPosition(int dice_number) // Ставит игрока на новую клетку
+        public void SetPosition(int cage_number)
         {
-            this._Position_Cage += dice_number;
-            if(this._Position_Cage >= Utils._size)
+            int[] pos = Utils.GetGridPositionByCageNumber(cage_number);
+            this._Position = pos;
+            this._Position_Cage = cage_number;
+
+            if (Utils.GetPlayersInCage(this._Position_Cage).Count >= 2)
             {
-                this._Position_Cage = Utils._size;
-                MessageBox.Show($"{this._Name} is winner!");
-                return;
+                AlterFormat(true);
+            }
+            else
+            {
+                AlterFormat(false);
+            }
+            
+            this._label.SetValue(Grid.RowProperty, pos[0]);
+            this._label.SetValue(Grid.ColumnProperty, pos[1]);
+        }
+
+        public void SetPosition(int row, int column)
+        {
+            this._Position_Cage = Utils.GetCageNumberByGrid(row, column);
+            if (Utils.GetPlayersInCage(this._Position_Cage).Count >= 2)
+            {
+                AlterFormat(true);
+            }
+            else
+            {
+                AlterFormat(false);
             }
 
-            this._Position = new int[] { Utils._list_cages[_Position_Cage][0], Utils._list_cages[_Position_Cage][1] };
-            this._label.SetValue(Grid.ColumnProperty, Utils._list_cages[_Position_Cage][0]);
-            this._label.SetValue(Grid.RowProperty, Utils._list_cages[_Position_Cage][1]);
+            this._Position = new int[] { row, column };
+            this._label.SetValue(Grid.RowProperty, row);
+            this._label.SetValue(Grid.ColumnProperty, column);
+        }
+
+        public void AddPosition(int dice_number)
+        {
+            this._Position_Cage += dice_number;
+            if(this._Position_Cage >= Utils._size * Utils._size)
+            {
+                // Win event
+                this._Position_Cage = Utils._size * Utils._size;
+                MessageBox.Show($"{this._Name} is winner!");
+            }
+
+            if (Utils.GetPlayersInCage(this._Position_Cage).Count >= 2)
+            {
+                AlterFormat(true);
+            }
+            else
+            {
+                AlterFormat(false);
+            }
+
+            this._Position = Utils.GetGridPositionByCageNumber(this._Position_Cage);
+            this._label.SetValue(Grid.RowProperty, this._Position[0]);
+            this._label.SetValue(Grid.ColumnProperty, this._Position[1]);
+        }
+
+        private void AlterFormat(bool enabled)
+        {
+            List<Player> players = Utils.GetPlayersInCage(this._Position_Cage);
+            if (enabled)
+            {
+                foreach (Player item in players)
+                {
+                    item._label.VerticalAlignment = item._VerticalAlignment;
+                    item._label.HorizontalAlignment = item._HorizontalAlignment;
+                }
+            }
+            else
+            {
+                foreach (Player item in players)
+                {
+                    item._label.VerticalAlignment = VerticalAlignment.Center;
+                    item._label.HorizontalAlignment = HorizontalAlignment.Center;
+                }
+            }
         }
     }
 }
--- SnakesAndLadders/Utils.cs
+++ SnakesAndLadders/Utils.cs
@@ -12,8 +12,41 @@ namespace SnakesAndLadders
     {
         public static Map _map { get; set; }
         public static List<Player> _players = new List<Player>();
-        public static Dictionary<int, int[]> _list_cages = new Dictionary<int, int[]> { }; // хэширование клеток поля, имеет след. представление [key: index, value: [column, row]]
-                                                                                           // где index - номер клетки, column - номер колонки, row - номер столбца
-        public const int _size = 22; // размер игрового поля
+        public const int _size = 5; // размер игрового поля, расчитывается по формуле матрицы N * N
+        public static int[,] _cachemap = new int[_size, _size]; // хэширование клеток поля, имеет след. представление [index, [column, row]]
+                                                                       // где index - номер клетки, column - номер колонки, row - номер столбца
+        public static int GetCageNumberByGrid(int row, int column)
+        {
+            return _cachemap[row, column];
+        }
+
+        public static int[] GetGridPositionByCageNumber(int number)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    if(_cachemap[i, j] == number)
+                    {
+                        int[] arr = { i, j };
+                        return arr;
+                    }
+                }
+            }
+            return new int[] {-1, -1};
+        }
+
+        public static List<Player> GetPlayersInCage(int number)
+        {
+            List<Player> players = new List<Player>();
+            foreach (Player item in _players)
+            {
+                if (item._Position_Cage == number)
+                {
+                    players.Add(item);
+                }
+            }
+            return players;
+        }
     }
 }
