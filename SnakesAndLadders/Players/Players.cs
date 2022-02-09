dee24258843617e6a4ba60e8f7a1caa5d06b7964
WinsomeQuill <artemqa.belyakov07@gmail.com>
2022-02-09 12:19:31 +0700

    Update

---
+++ SnakesAndLadders/Players/Players.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesAndLadders.Players
+{
+    public class Players
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int[] Pos { get; set; }
+        public BadImageFormatException Image { get; set; }
+        public void Romanza(string name, BadImageFormatException image)
+        {
+            this.Name = name;
+            this.Image = image;
+        }
+        private void SetPosition(int column, int row)
+        {
+            Pos[0] = column;
+            Pos[1] = row;
+        }
+        private void ResetPosition()
+        {
+            Pos[0] = 0;
+            Pos[1] = 0;
+        }
+    }
+}
---
+++ SnakesAndLadders/SelectionMaps/SelectMaps.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SnakesAndLadders.SelectionMaps
+{
+    /// <summary>
+    /// Логика взаимодействия для SelectMaps.xaml
+    /// </summary>
+    public partial class SelectMaps : Window
+    {
+        public SelectMaps()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            switch (ComboBoxSelectMap.SelectedIndex)
+            {
+                case 0:
+                    Map1.Visibility = Visibility.Visible;
+                    Map2.Visibility = Visibility.Collapsed;
+                    Map3.Visibility = Visibility.Collapsed;
+                    break;
+                case 1:
+                    Map1.Visibility = Visibility.Collapsed;
+                    Map2.Visibility = Visibility.Visible;
+                    Map3.Visibility = Visibility.Collapsed;
+                    break;
+                case 2:
+                    Map1.Visibility = Visibility.Collapsed;
+                    Map2.Visibility = Visibility.Collapsed;
+                    Map3.Visibility = Visibility.Visible;
+                    break;
+            }
+
+        }
+    }
+}
